12d44c29ba2ab295561ae419fb199b1a4bd02fea
Kasador <hunterstevenshaw@gmail.com>
2025-03-21 15:00:46 -0700

    ⚠️Fixing Dev Sever >> Added error handling for fetching on client-side.

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -9,8 +9,8 @@ const Weather = () => {
     useEffect(() => { // use effect to run this first
         const fetchWeatherData = async () => { // if we are fetching from API we need async to wait for data 
             try { // fetch and store weather data
-                const data = await getWeatherData(); // now run my function from the services 
-                setWeather(data); // store data into my weather state mana
+                const fetchData = await getWeatherData(); // now run my function from the services 
+                setWeather(fetchData.data); // store data into my weather state mana
             } catch (error) { // store error message
                 setError(error);
             } finally { // no longer loading
@@ -18,10 +18,11 @@ const Weather = () => {
             }
         };
 
+        console.log(weather, 'data');
+
         fetchWeatherData(); // try and fetch the weather data w/ error handling
     }, []); // this runs once the component mounts
 
-    console.log(weather, 'data');
 
     if (loading) return <div>Loading... Please Wait.</div>
     if (error) return <div>Oh no! There was an error: {error}</div>
@@ -30,12 +31,12 @@ const Weather = () => {
         <>
             <h1>Weather Data Testing:</h1>
             <div>
-            <h1>Weather in {weather.data.name}</h1>
-            <p>Temperature: {weather.data.main.temp} K</p>
-            <p>Condition: {weather.data.weather[0].description}</p>
-            <p>Feels Like: {weather.data.main.feels_like} K</p>
-            <p>Wind Speed: {weather.data.wind.speed} m/s</p>
-            <p>Humidity: {weather.data.main.humidity}%</p>
+            <h1>Weather in {weather.name}</h1>
+            <p>Temperature: {weather.main.temp} K</p>
+            <p>Condition: {weather.weather[0].description}</p>
+            <p>Feels Like: {weather.main.feels_like} K</p>
+            <p>Wind Speed: {weather.wind.speed} m/s</p>
+            <p>Humidity: {weather.main.humidity}%</p>
         </div>
         </>
     )
