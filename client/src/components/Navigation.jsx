be6c9fbf07617bfc489e47479fc2c81d0e03074b
Kasador <hunterstevenshaw@gmail.com>
2025-03-29 14:44:44 -0700

    🎨🖌️🌦️ Added & Styled Nav Header Graphic.

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+    return (
+        <nav>
+            <header>
+                <div id="firstAlertContainer">
+                    <h1>F1RST </h1>
+                    <span>/</span>
+                    <h1 id='alertHeader'>ALERT</h1>
+                </div>
+                <hr></hr>
+                <h2>WEATHER</h2>
+            </header>
+        </nav>
+    )
+}
+
+export default Navigation;
